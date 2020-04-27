96cc6a54449b473a830d05bbfb2b01d957f6b836
Manu1ND <manjunath2000@hotmail.com>
2020-04-27 12:37:08 +0530

    removed comments

--- score_db.py
+++ score_db.py
@@ -3,11 +3,7 @@ from sqlite3 import Error
  
  
 def create_connection(db_file):
-    """ create a database connection to the SQLite database
-        specified by db_file
-    :param db_file: database file
-    :return: Connection object or None
-    """
+  
     conn = None
     try:
         conn = sqlite3.connect(db_file)
@@ -18,12 +14,7 @@ def create_connection(db_file):
  
  
 def add_data(conn, data):
-    """
-    Create a new project into the projects table
-    :param conn:
-    :param project:
-    :return: project id
-    """
+    
     sql = ''' INSERT INTO score(player_name,player_score,comp_score)
               VALUES(?,?,?) '''
     cur = conn.cursor()
@@ -36,6 +27,6 @@ def save_score(player_name, player_score, comp_score):
     # create a database connection
     conn = create_connection(database)
     with conn:
-        # create a new project
+
         data = (player_name, player_score, comp_score);
         add_data(conn, data)
